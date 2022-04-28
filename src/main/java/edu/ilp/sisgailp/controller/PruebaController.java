5ba5d1569cc3b28cb1583e5a644f0b303e729db4
aldaircollin <aldair.hc2001@gmail.com>
2022-04-27 22:27:34 -0500

    first commit

---
+++ src/main/java/edu/ilp/sisgailp/controller/PruebaController.java
@@ -0,0 +1,21 @@
+package edu.ilp.sisgailp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("login")
+public class PruebaController {
+
+    @GetMapping("/saludo")
+    public String saludo(){
+        return "Hola amig@s desde Ayacucho - perú";
+    }
+
+    @GetMapping("/bienvenido")
+    public String identificacion(@RequestParam String datos){
+        return "Bienvenido a la Pontificia: " + datos;
+    }
+}
